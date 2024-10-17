690fa4594ebc0dfc84ecf53880b036018a35e672
feli-verdugo <feli.verdugo@duocuc.cl>
2024-10-17 13:42:36 -0300

    django con views y urls arreglados parcialmente xd

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
-from . import views  # Importa las vistas desde tu archivo views.py
+from . import views  
 
 urlpatterns = [
     path('', views.home, name='home'),  # Ruta para la vista 'home'
+
+    path('login/', views.login, name='login'),  
+
+    path('registro/', views.registro, name='registro'),  
+    
+    path('admin/', views.admin_view, name='admin'),
+
+    path('formulario retiro/', views.formulario_retiro, name='formulario retiro'),
+
+    path('retiros pendientes/', views.retiros_pendientes, name='retiros pendientes'), 
+
+    path('loading/', views.test_loading, name='loading'), 
+
+    path('historial usuario/', views.historial_usuario, name='historial usuario'), 
+
+    path('boleta/', views.boleta, name='boleta'), 
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,3 +7,27 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+
+def admin_view(request):
+    return render(request, 'admin.html')
+
+def formulario_retiro(request):
+    return render(request, 'formulario-retiro.html')
+
+def retiros_pendientes(request):
+    return render(request, 'retiros-pendientes.html')
+
+def test_loading(request):
+    return render(request, 'test-loading.html')
+
+def historial_usuario(request):
+    return render(request, 'usuario-historial.html')
+
+def boleta(request):
+    return render(request, 'boleta.html')
